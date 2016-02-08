f749c23f65b2daf6b1db64aaee955b7e735d589f
phstearns <phstearns13@gmail.com>
2016-02-08 14:29:45 -0500

    Updated from Brython Server: 2/8/2016 2:29:45 PM

--- stringjumble.py
+++ stringjumble.py
@@ -1,7 +1,7 @@
 """
 stringjumble.py
-Author: <your name>
-Credit: <sources>
+Author: Payton Stearns
+Credit: 
 
 Assignment:
 
@@ -25,3 +25,4 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+ost = input("Please enter a string of text (the bigger the better): ")
