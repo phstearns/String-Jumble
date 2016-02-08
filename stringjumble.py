821b5ceb220f3b773bc5ba4eef20a1aea2b93a66
phstearns <phstearns13@gmail.com>
2016-02-08 14:30:54 -0500

    Updated from Brython Server: 2/8/2016 2:30:54 PM

--- stringjumble.py
+++ stringjumble.py
@@ -26,3 +26,4 @@ handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
 ost = input("Please enter a string of text (the bigger the better): ")
+print("You entered" + ost + ". Now Jumble it: "
