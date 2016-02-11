366224449a97c5353389e20f10bb6ac388ad2d0d
phstearns <phstearns13@gmail.com>
2016-02-10 19:58:30 -0500

    Updated from Brython Server: 2/10/2016 7:58:32 PM

--- stringjumble.py
+++ stringjumble.py
@@ -47,5 +47,5 @@ for word in sost:
     q=0
     while q<len(word):
         r = (str(word[len(word)-q-1]))
-        print("{0: <3} ".format(r), end="")
+        print("{0: <3} ".format(r), end= "")
         q=q+1
